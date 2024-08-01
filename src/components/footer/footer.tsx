098ab44e00c906b4359881fe94e0c5105d3e7053
jenjarus <jenja-oskol@yandex.ru>
2024-08-01 22:15:14 +0300

    Подготовка к работе Добавил стили, картинки. Изменил meta-теги. Убрал лишнее

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="copyright">Jenjarus 2021</div>
+      </div>
+    </footer>
+  );
+};
