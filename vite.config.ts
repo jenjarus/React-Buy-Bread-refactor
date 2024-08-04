ce4cdce2a0e063ee9941a8385106c72e9bc77229
jenjarus <jenja-oskol@yandex.ru>
2024-08-04 19:42:11 +0300

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src')
+      '@' : './src/*',
     }
   }
 })
